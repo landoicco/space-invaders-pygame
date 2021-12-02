934d2fb8c77e5e3b941136961d8143f68241e728
Lando Icaza <landoicco@gmail.com>
2021-12-02 10:39:08 -0600

    First commit!

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        clock.tick(60)
